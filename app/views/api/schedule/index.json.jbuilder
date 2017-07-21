18e2cb3c6b8d1043450136e7172fe48bc09fb30d
Spencer Chan <schan1031@gmail.com>
2017-07-21 10:11:33 -0700

    pulling all schedulse and players on related teams

--- app/views/api/schedule/index.json.jbuilder
+++ app/views/api/schedule/index.json.jbuilder
@@ -1,5 +1,5 @@
 @schedules.each do |game|
-  json.set! game.date do
+  json.set! game.id do
     json.extract! game, :date, :home_id, :away_id
   end
 end
--- frontend/components/playerlist/player_list.jsx
+++ frontend/components/playerlist/player_list.jsx
@@ -5,22 +5,42 @@ export default class PlayerList extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    const teams = this.props.schedule;
-    
+  componentDidUpdate(prevProps) {
+    if (Object.keys(prevProps.schedule).length === 0) {
+      const teams = this.props.schedule;
+      const keys = Object.keys(teams);
+      const out = [];
+
+      for (let i = 0; i < keys.length; i++) {
+        const home = teams[keys[i]].home_id;
+        const away = teams[keys[i]].away_id;
+        if (!out.includes(home)) {
+          out.push(home);
+        }
+
+        if (!out.includes(away)) {
+          out.push(away);
+        }
+      }
+      this.props.fetchPlayers({
+        team_ids: out
+      });
+    }
+
   }
 
   render() {
-    console.log(this.props);
-    if (this.props.players) {
+    console.log(this.props.players);
+    if (Object.keys(this.props.players).length === 0) {
       return(
         <div></div>
       );
     } else {
       const p1 = this.props.players[Object.keys(this.props.players)[0]];
+      console.log(p1);
       return (
         <div>
-          {p1}
+          {p1.name} here it is
         </div>
       );
     }
